1b23bec3ef32c267eebb0020b2461e1adb5fc90b
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-11 17:03:55 +0000

    Create Domain Models for Patients

---
+++ StLukesMedicalApp.API/Models/Domain/Patient.cs
@@ -0,0 +1,17 @@
+﻿namespace StLukesMedicalApp.API.Models.Domain
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+        public string Diagnosis { get; set; }
+        public string PatientType { get; set; }
+
+    }
+}
--- StLukesMedicalApp.API/obj/Debug/net9.0/StLukesMedicalApp.API.AssemblyInfo.cs
+++ StLukesMedicalApp.API/obj/Debug/net9.0/StLukesMedicalApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+464575ee15e5cd33895af38445ea9d3301856dac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d87d03808d83499ed67f0b358569b9d5b8ef2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
