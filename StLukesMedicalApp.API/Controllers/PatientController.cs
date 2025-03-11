39bfeb8b5dcecb2649b98c08372cf9282574e899
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-11 18:26:21 +0000

    Created All REST API Methods for Patients

--- StLukesMedicalApp.API/Controllers/PatientController.cs
+++ StLukesMedicalApp.API/Controllers/PatientController.cs
@@ -53,5 +53,145 @@ namespace StLukesMedicalApp.API.Controllers
 
             return Ok(response);
         }
+
+
+        // Get All Patients
+        [HttpGet]
+        public async Task<IActionResult> GetAllPatients()
+        {
+            var patients = await patientRepository.GetAllAsync();
+
+            // map Domain model to DTO
+            var response = new List<PatientDto>();
+            foreach (var patient in patients)
+            {
+                response.Add(new PatientDto
+                {
+                    Id = patient.Id,
+                    FirstName = patient.FirstName,
+                    LastName = patient.LastName,
+                    Email = patient.Email,
+                    ContactNumber = patient.ContactNumber,
+                    Sex = patient.Sex,
+                    Age = patient.Age,
+                    Address = patient.Address,
+                    Diagnosis = patient.Diagnosis,
+                    PatientType = patient.PatientType,
+                });
+            }
+
+            return Ok(response);
+        }
+
+        // Get Patient By ID
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetPatientById([FromRoute] Guid id)
+        {
+            // get patient by Id
+            var patient = await patientRepository.GetByIdAsync(id);
+
+            // check if patient is null
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to DTO
+            var response = new PatientDto
+            {
+                Id = patient.Id,
+                FirstName = patient.FirstName,
+                LastName = patient.LastName,
+                Email = patient.Email,
+                ContactNumber = patient.ContactNumber,
+                Sex = patient.Sex,
+                Age = patient.Age,
+                Address = patient.Address,
+                Diagnosis = patient.Diagnosis,
+                PatientType = patient.PatientType,
+            };
+
+            return Ok(response);
+        }
+
+        // Update Patient
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdatePatient([FromRoute] Guid id, UpdatePatientRequestDto request)
+        {
+            // convert dto to domain model
+            var patient = new Patient
+            {
+                Id = id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                ContactNumber = request.ContactNumber,
+                Sex = request.Sex,
+                Age = request.Age,
+                Address = request.Address,
+                Diagnosis = request.Diagnosis,
+                PatientType = request.PatientType,
+            };
+
+            // update patient
+            patient = await patientRepository.UpdateAsync(patient);
+
+            // check if patient is null
+            if (patient == null) 
+            {
+                return NotFound();
+            }
+
+            // map domain model to Dto
+            var response = new PatientDto
+            {
+                Id = patient.Id,
+                FirstName = patient.FirstName,
+                LastName = patient.LastName,
+                Email = patient.Email,
+                ContactNumber = patient.ContactNumber,
+                Sex = patient.Sex,
+                Age = patient.Age,
+                Address = patient.Address,
+                Diagnosis = patient.Diagnosis,
+                PatientType = patient.PatientType,
+            };
+
+            return Ok(response);
+        }
+
+        // Delete Patient
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeletePatient([FromRoute] Guid id)
+        {
+            // delete patient
+            var patient = await patientRepository.DeleteAsync(id);
+
+            //check if patient is null
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new PatientDto
+            { 
+                Id = patient.Id,
+                FirstName = patient.FirstName,
+                LastName = patient.LastName,
+                Email = patient.Email,
+                ContactNumber = patient.ContactNumber,
+                Sex = patient.Sex,
+                Age = patient.Age,
+                Address = patient.Address,
+                Diagnosis = patient.Diagnosis,
+                PatientType = patient.PatientType,
+            };
+
+            return Ok(response);
+        }
     }
 }
--- StLukesMedicalApp.API/Repositories/Implementation/PatientRepository.cs
+++ StLukesMedicalApp.API/Repositories/Implementation/PatientRepository.cs
@@ -1,4 +1,5 @@
-﻿using StLukesMedicalApp.API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using StLukesMedicalApp.API.Data;
 using StLukesMedicalApp.API.Models.Domain;
 using StLukesMedicalApp.API.Repositories.Interface;
 
@@ -18,6 +19,49 @@ namespace StLukesMedicalApp.API.Repositories.Implementation
            await dbContext.Patients.AddAsync(patient);
            await dbContext.SaveChangesAsync();
            return patient;
+        }
+
+        // Get All Patients
+        public async Task<IEnumerable<Patient>> GetAllAsync()
+        {
+            return await dbContext.Patients.ToListAsync();
+        }
+
+        // Get Patient By ID
+        public async Task<Patient?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Patients.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        // Update Patient
+        public async Task<Patient?> UpdateAsync(Patient patient)
+        {
+           var existingPatient = await dbContext.Patients.FirstOrDefaultAsync( x=>x.Id == patient.Id);
+
+            if (existingPatient != null) 
+            { 
+                dbContext.Entry(existingPatient).CurrentValues.SetValues(patient);
+                await dbContext.SaveChangesAsync();
+                return patient;
+            }
+
+            return null;
+
+        }
+
+        // Delete Patient
+        public async Task<Patient?> DeleteAsync(Guid id)
+        {
+           var existingPatient = dbContext.Patients.FirstOrDefault(x => x.Id == id);
+
+            if (existingPatient is null) 
+            {
+                return null;
+            }
+
+            dbContext.Patients.Remove(existingPatient);
+            await dbContext.SaveChangesAsync();
+            return existingPatient;
         }
     }
 }
