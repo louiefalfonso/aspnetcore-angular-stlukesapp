a13a9449f70ac279a4072376e2e14546a6ba6bc2
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-21 19:05:04 +0000

    Added Auth Controller

---
+++ StLukesMedicalApp.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StLukesMedicalApp.API.Models.DTO;
+using StLukesMedicalApp.API.Repositories.Interface;
+
+namespace StLukesMedicalApp.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
+
+        public AuthController(
+        UserManager<IdentityUser> userManager, 
+        ITokenRepository tokenRepository
+           )
+        {
+            this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterRequestDto request)
+        {
+
+        }
+    }
+}
