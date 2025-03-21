2dca56a0c9710dc47215844876824e31bab7ef04
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-21 19:12:08 +0000

    Setup .gitignore

--- StLukesMedicalApp.API/obj/Debug/net9.0/StLukesMedicalApp.API.AssemblyInfo.cs
+++ StLukesMedicalApp.API/obj/Debug/net9.0/StLukesMedicalApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13a9449f70ac279a4072376e2e14546a6ba6bc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22fa6d567d30442ecbf26cc82fc35f9fbd6878f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StLukesMedicalApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
