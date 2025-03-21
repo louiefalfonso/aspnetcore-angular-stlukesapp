dd47e6a267355e47bdecd4d6d3d19816e3c974a9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-21 23:35:06 +0000

    Setup Patient UI Components

---
+++ StLukesMedicalApp.UI/src/app/features/patient/models/patient.models.ts
@@ -0,0 +1,12 @@
+export interface Patient {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    contactNumber: string;
+    sex: string;
+    age: string;
+    address: string;
+    diagnosis: string;
+    patientType: string;
+}
---
+++ StLukesMedicalApp.UI/src/app/features/patient/models/update-patient-request.model.ts
@@ -0,0 +1,11 @@
+export interface UpdatePatientRequest{
+    firstName: string;
+    lastName: string;
+    email: string;
+    contactNumber: string;
+    sex: string;
+    age: string;
+    address: string;
+    diagnosis: string;
+    patientType: string;
+}
