319f2e447069184ed903f1f88a0e8484ff49a119
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-22 12:42:26 +0000

    Setup Responsive Header & Sidebar Content w/ Material UI

--- StLukesMedicalApp.UI/src/app/app.routes.ts
+++ StLukesMedicalApp.UI/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/public/home/home.component';
+import { AdmissionComponent } from './features/public/admission/admission.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+        path: "",
+        component: HomeComponent
+   },
+   {
+        path: "admissions",
+        component: AdmissionComponent
+   },
+];
--- StLukesMedicalApp.UI/src/app/core/components/navbar/navbar.component.ts
+++ StLukesMedicalApp.UI/src/app/core/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
