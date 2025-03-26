54ca448b15341b42b54e54b7fe61d552607ac3d0
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-26 22:13:29 +0000

    Update Navbar & Patient Components

--- StLukesMedicalApp.UI/src/app/app.routes.ts
+++ StLukesMedicalApp.UI/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PatientListComponent } from './features/patient/patient-list/patient-list.component';
+import { HomeComponent } from './features/public/home/home.component';
+import { AddPatientComponent } from './features/patient/add-patient/add-patient.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: HomeComponent,
+        
+    },
+    {
+        path:"admin/patients",
+        component: PatientListComponent, 
+    },
+    {
+        path:"admin/patients/add",
+        component: AddPatientComponent, 
+    },
+];
