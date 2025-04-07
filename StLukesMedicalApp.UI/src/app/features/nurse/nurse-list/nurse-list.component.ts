95892c8b0e6701bbb6e8281ba6eefb55e1e23f58
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-07 16:59:22 +0100

    Update UI Components for Filter, Sorting & Search Features

--- StLukesMedicalApp.UI/src/app/features/nurse/nurse-list/nurse-list.component.ts
+++ StLukesMedicalApp.UI/src/app/features/nurse/nurse-list/nurse-list.component.ts
@@ -20,7 +20,7 @@ export class NurseListComponent implements OnInit {
   totalCount?: number;
   list: number[] = [];
   pageNumber = 1;
-  pageSize = 10;
+  pageSize = 5;
 
   // add constructor
   constructor(
@@ -51,6 +51,19 @@ export class NurseListComponent implements OnInit {
   // implement search
     onSearch(query: string) {
       this.nurses$ = this.nurseService.getAllNurses(query);
+  }
+
+  //implement reset
+  onReset(queryText: HTMLInputElement): void {
+    queryText.value = ''; 
+    this.pageNumber = 1; 
+    this.nurses$ = this.nurseService.getAllNurses(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    ); 
   }
 
  // implement sorting
